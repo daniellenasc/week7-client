f3508a9ed4bad2e2b8181f90dea83010854111cc
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-12-19 21:45:39 +0100

    profile pic done

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -13,8 +13,35 @@ function SignUpPage() {
     confirmPassword: "",
   });
 
+  //state para guardar a imagem escolhida pelo usuário
+  const [img, setImg] = useState();
+
   function handleChange(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function handleImage(e) {
+    //console.log(e.target.files[0]); //é um array-like object -> são as informações da foto que deverão ser enviadas para a rota de upload do back-end
+    setImg(e.target.files[0]);
+  }
+
+  // função assíncrona para fazer o upload da foto
+  async function handleUpload(e) {
+    try {
+      const uploadData = new FormData(); //para formatar a imagem -> A interface FormData fornece uma maneira fácil de construir um conjunto de pares chave/valor representando campos de um elemento form e seus valores, os quais podem ser facilmente enviados utilizado o método send() do XMLHttpRequest. Essa interface utiliza o mesmo formato que um form utilizaria se o tipo de codificação estivesse configurado como "multipart/form-data".
+      uploadData.append("picture", img); //Acrescenta um novo valor em uma chave existente dentro de um objeto FormData, ou adiciona a chave se ela ainda não existir.
+      //"picture" -> vem da rota do backend
+      //valor -> state img
+
+      const response = await api.post("/uploadImage/upload", uploadData);
+
+      //console.log(uploadData);
+
+      //a função retorna uma url
+      return response.data.url;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleSubmit(e) {
@@ -26,9 +53,12 @@ function SignUpPage() {
       return;
     }
 
+    //chamar a função handleUpload()
+    const imgUrl = await handleUpload();
+
     //disparo a requisição de cadastro para o meu servidor
     try {
-      await api.post("/user/sign-up", form);
+      await api.post("/user/sign-up", { ...form, profilePic: imgUrl });
 
       navigate("/login");
     } catch (error) {
@@ -87,6 +117,12 @@ function SignUpPage() {
             value={form.confirmPassword}
             onChange={handleChange}
           />
+        </Form.Group>
+
+        {/* o input do tipo 'file' não tem value */}
+        <Form.Group className="mb-3">
+          <Form.Label>Foto de Perfil</Form.Label>
+          <Form.Control type="file" onChange={handleImage} />
         </Form.Group>
 
         <Button className="my-3" variant="dark" type="submit">
